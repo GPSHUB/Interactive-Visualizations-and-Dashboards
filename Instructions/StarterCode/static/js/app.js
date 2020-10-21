11d5dc0835dcb0bc51d20641add3e754d1bb4b8f
GiamSigaud <giam_sigaud@hotmail.com>
2020-10-21 14:40:57 -0500

    10/21/20 2:40pm CST

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,36 +1,109 @@
-// Use the D3 library to read in samples.json. I called JSON data 'importData'
-d3.json("samples.json").then((importData) =>{
-    // console.log(importData);
-    let data = importData;
-
-// Isolating test subject names for the dropdown menu
-    let subjectNames = data.names;
-    // console.log(subjectNames);
-
-// Appending looped test subject names to the dropdown menu ID in the index.html
-// https://stackoverflow.com/questions/43121679/how-to-append-option-into-select-combo-box-in-d3
-    subjectNames.forEach((name) => {
-        d3.select("#selDataset").append('option').text(name)})
-
-// Isolating objects for insertion into demo info table
-    let demoInfo = data.metadata.filter(sample => sample.id)[0];
-    // console.log(demoInfo);
-
-// class act 14 / day 2 / activity 7 line 15 // append("p") appends new paragraph
-// Appending looped metadata object entries to the demo info panel in the index.html
-    Object.entries(demoInfo).forEach(
-        ([key,value]) => d3.select("#sample-metadata").append("p").text(`${key}: ${value}`)
-    );
-
-    // Isolating objects for bar chart
-    function barChart () {
-        let dropInput = d3.select("#selDataset");
-
-        let dropValue = dropInput.property("value");
-        console.log(dropValue);
-    
-//NEED TO CONNECT DROP DOWN TO DEMO INFO VIA EVENT LISTENERS AND HANDLERS
-
-///////////////// 00  /////////////////
-
-}});
+function buildCharts(id) {
+    d3.json("samples.json")
+        .then(function (importData) {
+            console.log(importData)
+            
+            let data = importData;
+            let filteredData = importData.samples.filter(sample => sample.id === id);
+            //demo meta importData not being populated
+            let demos = data.metadata.filter(meta => meta.id == id);
+            // console.log("data metadata", data.metadata)
+            // console.log("my id is", id)
+            // console.log('demo importData is', demos)
+            //clear html for next sample
+            let pbody = d3.select("#sample-metadata");
+            pbody.html('')
+            Object.entries(demos[0]).forEach(
+                ([key, value]) => d3.select("#sample-metadata").append("p").text(`${key}: ${value}`)
+            );
+            // console.log(filteredData[0].otu_ids)
+            // console.log(filteredData[0].otu_labels)
+            let topTenSV = [filteredData[0].sample_values[0], filteredData[0].sample_values[1], filteredData[0].sample_values[2], filteredData[0].sample_values[3], filteredData[0].sample_values[4], filteredData[0].sample_values[5], filteredData[0].sample_values[6], filteredData[0].sample_values[7], filteredData[0].sample_values[8], filteredData[0].sample_values[9]]
+            let topTenOTU = [filteredData[0].otu_ids[0], filteredData[0].otu_ids[1], filteredData[0].otu_ids[2], filteredData[0].otu_ids[3], filteredData[0].otu_ids[4], filteredData[0].otu_ids[5], filteredData[0].otu_ids[6], filteredData[0].otu_ids[7], filteredData[0].otu_ids[8], filteredData[0].otu_ids[9]]
+            let topTenOTULabels = [filteredData[0].otu_labels[0], filteredData[0].otu_labels[1], filteredData[0].otu_labels[2], filteredData[0].otu_labels[3], filteredData[0].otu_labels[4], filteredData[0].otu_labels[5], filteredData[0].otu_labels[6], filteredData[0].otu_labels[7], filteredData[0].otu_labels[8], filteredData[0].otu_labels[9]]
+            let labelArray = []
+            //concat OTU to array for labels
+            for (let i = 0; i < 10; i++) {
+                labelArray.push("OTU " + filteredData[0].otu_ids[i])
+                // console.log("label array ", labelArray)
+            }
+            // console.log("labels array complete is ", labelArray)
+            // console.log("top ten is:", topTenOTU)
+            // Create the Trace
+            let trace1 = {
+                x: topTenSV,
+                y: labelArray,
+                mode: 'markers',
+                marker: { size: 16 },
+                text: topTenOTULabels,
+                type: 'bar',
+                orientation: 'h'
+            };
+            // console.log("first value is :", filteredData[0].sample_values[0])
+            // // Create the importData array for our plot
+            let data2 = [trace1];
+            // // Define our plot layout
+            let layout = {
+                title: "OTU vs Sample Values",
+                xaxis: { title: "Sample Values" },
+                yaxis: { title: "OTU IDs" },
+                showlegend: false,
+                // xaxis: {
+                //     tickangle: -45
+                //   },
+                // yaxis: {
+                //     zeroline: false,
+                //     gridwidth: 2
+                //   },
+            };
+            // // Plot the chart to a div tag with id "bar-plot"
+            Plotly.newPlot("bar", data2, layout, { displayModeBar: true });
+            //BUBBLE PLOT 
+            let bubbleTrace = {
+                x: topTenOTU, //[0, 1, 2, 3, 4, 5, 6],
+                y: topTenSV, //[1, 9, 4, 7, 5, 2, 4],
+                mode: 'markers',
+                marker: {
+                    size: topTenSV,
+                    color: topTenOTU
+                },
+                text: topTenOTULabels
+            };
+            let bubbleData = [bubbleTrace];
+            let bubbleLayout = {
+                title: 'Marker Size',
+                showlegend: false,
+                height: 600,
+                width: 600
+            };
+            Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+        })
+}
+//sends new id in everytime drop down changes
+function optionChanged(dropDownValue) {
+    console.log("onchange dropdown value", dropDownValue)
+    buildCharts(dropDownValue)
+}
+function init() {
+    console.log("Initilize page")
+    let dropDownBtn = d3.select("#selDataset");
+    d3.json("samples.json")
+        .then(function (importData) {
+            console.log("test", importData)
+            let names = importData.names
+            names.forEach(name => {
+                dropDownBtn.append("option")
+                    .text(name)
+                    .attr("value", name)
+            })
+            importData.samples.forEach(sv => console.log(sv));
+            // for demographics area on load
+            let demo = importData.metadata.filter(sample => sample.id)[0];
+            console.log(demo);
+            Object.entries(demo).forEach(
+                ([key, value]) => d3.select("#sample-metadata").append("p").text(`${key}: ${value}`)
+            );
+            buildCharts(names[0])
+        });
+}
+init();
