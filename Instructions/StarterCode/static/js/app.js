7d3188e8ba6304b894423411c3ba3b6a74e65991
GiamSigaud <giam_sigaud@hotmail.com>
2020-10-20 19:04:59 -0500

    10/20/20 7:04pm cst

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,11 +1,11 @@
 // Use the D3 library to read in samples.json. I called JSON data 'importData'
 d3.json("samples.json").then((importData) =>{
-    console.log(importData);
+    // console.log(importData);
     let data = importData;
 
 // Isolating test subject names for the dropdown menu
     let subjectNames = data.names;
-    console.log(subjectNames);
+    // console.log(subjectNames);
 
 // Appending looped test subject names to the dropdown menu ID in the index.html
 // https://stackoverflow.com/questions/43121679/how-to-append-option-into-select-combo-box-in-d3
@@ -14,13 +14,23 @@ d3.json("samples.json").then((importData) =>{
 
 // Isolating objects for insertion into demo info table
     let demoInfo = data.metadata.filter(sample => sample.id)[0];
-    console.log(demoInfo);
+    // console.log(demoInfo);
 
-// class act 14 / day 2 / activity 7 line 15 // append("p") means append new paragraph
+// class act 14 / day 2 / activity 7 line 15 // append("p") appends new paragraph
 // Appending looped metadata object entries to the demo info panel in the index.html
     Object.entries(demoInfo).forEach(
         ([key,value]) => d3.select("#sample-metadata").append("p").text(`${key}: ${value}`)
     );
-});
 
+    // Isolating objects for bar chart
+    function barChart () {
+        let dropInput = d3.select("#selDataset");
+
+        let dropValue = dropInput.property("value");
+        console.log(dropValue);
+    
 //NEED TO CONNECT DROP DOWN TO DEMO INFO VIA EVENT LISTENERS AND HANDLERS
+
+///////////////// 00  /////////////////
+
+}});
